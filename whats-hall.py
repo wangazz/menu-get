5191fab694217aedba063fa436aaa43808432747
Andy Wang <34005234+wangazz@users.noreply.github.com>
2019-01-15 18:45:13 +0000

    Deleted unused files

--- whats-hall.py
+++ whats-hall.py
@@ -1,16 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 
+# authentication
 username = 'some3985'
 password = 'password'
 
 driver = webdriver.PhantomJS('phantomjs.exe')
 
+# enter credentials into Shibboleth identity provider
 driver.get('https://meals.some.ox.ac.uk/pallmenu.php')
 driver.find_element_by_id('username').send_keys(username)
 driver.find_element_by_id('password').send_keys(password)
 driver.find_element_by_name('Submit').click()
-
 driver.find_element_by_class_name('go_button').click()
+
+# find menu
 element = driver.find_element_by_tag_name('table')
 print(element.text)
