8224af2d3810a4810d039fc3c17d5719aa845959
Andy Wang <34005234+wangazz@users.noreply.github.com>
2019-01-15 21:05:18 +0000

    Working proof of concept

--- whats-hall.py
+++ whats-hall.py
@@ -1,10 +1,6 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.by import By
-from bs4 import BeautifulSoup
-import re
+# from bs4 import BeautifulSoup
 import pandas as pd
-import os
 
 # authentication
 username = 'some3985'
@@ -14,16 +10,40 @@ password = ''
 driver = webdriver.PhantomJS('phantomjs.exe')
 
 # enter credentials into Shibboleth identity provider
-driver.get('https://meals.some.ox.ac.uk/index.php')
-driver.find_element_by_id('username').send_keys(username)
-driver.find_element_by_id('password').send_keys(password)
-driver.find_element_by_name('Submit').submit()
-driver.find_element_by_class_name('go_button').click()
+try:
+    driver.get('https://meals.some.ox.ac.uk/index.php')
+    driver.find_element_by_id('username').send_keys(username)
+    driver.find_element_by_id('password').send_keys(password)
+    driver.find_element_by_name('Submit').submit()
+    driver.find_element_by_class_name('go_button').click()
 
-# go to menu page
-driver.get('https://meals.some.ox.ac.uk/pallmenu.php')
-driver.save_screenshot('screenshot.png')
+    # go to menu page
+    driver.get('https://meals.some.ox.ac.uk/pallmenu.php')
 
-soup = BeautifulSoup(driver.page_source, 'html.parser')
+except Exception as e:
+    driver.save_screenshot('screenshot.png')
+
+# soup = BeautifulSoup(driver.page_source, 'html.parser')
 
 df = pd.read_html(driver.page_source)[0]
+
+meal = {
+    'lunch' : 1, 
+    'lunch_v' : 2, 
+    'dinner' : 3, 
+    'dinner_v' : 4
+}
+
+day = {
+    'sun' : 2,
+    'mon' : 3,
+    'tue' : 4,
+    'wed' : 5,
+    'thu' : 6,
+    'fri' : 7,
+    'sat' : 8
+}
+
+# meals should be called in the form df[meal][day]
+# example
+df[meal['dinner']][day['thu']]
