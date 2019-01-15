a0d7c35e7539b2b19d00db8bdf4ad3aafe9b714c
Andy Wang <34005234+wangazz@users.noreply.github.com>
2019-01-15 19:54:06 +0000

    Gets menu

--- whats-hall.py
+++ whats-hall.py
@@ -1,19 +1,29 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import re
+import pandas as pd
+import os
 
 # authentication
 username = 'some3985'
-password = 'password'
+password = ''
 
+# driver = webdriver.Firefox()
 driver = webdriver.PhantomJS('phantomjs.exe')
 
 # enter credentials into Shibboleth identity provider
-driver.get('https://meals.some.ox.ac.uk/pallmenu.php')
+driver.get('https://meals.some.ox.ac.uk/index.php')
 driver.find_element_by_id('username').send_keys(username)
 driver.find_element_by_id('password').send_keys(password)
-driver.find_element_by_name('Submit').click()
+driver.find_element_by_name('Submit').submit()
 driver.find_element_by_class_name('go_button').click()
 
-# find menu
-element = driver.find_element_by_tag_name('table')
-print(element.text)
+# go to menu page
+driver.get('https://meals.some.ox.ac.uk/pallmenu.php')
+driver.save_screenshot('screenshot.png')
+
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+df = pd.read_html(driver.page_source)[0]
